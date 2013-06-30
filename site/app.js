877a9837833e755799c13ff56a8ebda608c06ad5
lepinay <lepinay@gmail.com>
2013-06-30 19:52:55 +0200

    config heroku

--- site/app.js
+++ site/app.js
@@ -10,4 +10,7 @@ app.get('/', function(req,res) {
     res.render('trouver_notre_eglise.ejs');
 });
 
-app.listen(8080);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
